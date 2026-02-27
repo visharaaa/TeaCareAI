47ede5359b1b4fec817765d5e3b1197c859a2fcc
Chinthaka-Sharuna <sharunadrc@gmail.com>
2026-02-27 16:39:34 +0530

    feat: enhance tea disease detection functionality with pixel counting and prediction methods

--- Tea Disease Identifier.py
+++ Tea Disease Identifier.py
@@ -1,22 +1,70 @@
+import torch
 from ultralytics import YOLO
 import cv2
-
-model = YOLO('./best.pt') 
-
-test_image_path = ["./data/processed/final_dataset/Blister Blight (Exobasidium vexans)/Blister_Blight_dt5_00052.jpg",
-                   "./data/processed/final_dataset/Helopeltis (Helopeltis theivora)/Helopeltis_dt5_12635.jpg"]
+import os
+import numpy as np
 
 
-results = model.predict(source=test_image_path, conf=0.25,boxes=False,save=True)
+#params => mask in numpy 2D array format
+#this  function count the number of pixels in the mask that are greater than 0, 
+# which indicates the presence of the detected object in those pixels. 
+# The function returns the total count of such pixels.
+def count_pixels(mask):
+    return int((mask > 0).sum())
 
 
-annotated_image = results[0].plot()
+#params=> class id of class and pixel count of that class
+#this function creates a numpy array of zeros with the same length as the number of classes (len(names)). 
+# It then increments the value at the index corresponding to the class_id by the pixel_count.
+def pixel_adder(class_id,pixel_count):
+    #print('class_id', class_id)
+    #print('pixel_count', pixel_count)
+    current=np.zeros((len(names)),dtype=int)
+    current[class_id]+=pixel_count
+    return current
 
 
-cv2.imshow("Tea Disease Detection", annotated_image)
+#param=> saved image name in uploaded_images folder
+#this function takes an image name as input, constructs the path to the image, and uses the YOLO model to make predictions on the image.
+# It initializes an array pixel_count_arr to store the pixel counts for each class to find thr infected area.
+def predict(img):
+    path=os.path.join('./uploaded_images',img)
+
+    results = model(source=path,conf=0.25)
+
+    #annotated_image = results[0].plot()
+    #print('result.masks', (results[0].masks).shape)
+
+    pixel_count_arr=np.zeros((len(names)),dtype=int)
+
+    for result in results:
+
+        infected_area_polgons=result.masks
+        #print('infected_area_polgons', infected_area_polgons.shape)
+        #print('infected_area_polgons', infected_area_polgons.shape[0])
+        for raw_mask_id in range(infected_area_polgons.shape[0]):
+            raw_mask_numpy = infected_area_polgons[raw_mask_id].data.cpu().numpy()[0]
+            #print('raw_mask_numpy', raw_mask_numpy.shape)
+            #print(raw_mask_id)
+            #cv2.imshow("Tea Disease Detection", annotated_image)
+
+            class_id=int(result.boxes.cls[raw_mask_id])
+            pixel_count_arr+=pixel_adder(class_id,count_pixels(raw_mask_numpy))
+
+            #cv2.imshow(f"Infected Area Mask {names[class_id]}", raw_mask_numpy * 255) # Multiply by 255 to visualize the mask
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows()
+
+    return pixel_count_arr
 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-print("Prediction finished!")
+model = YOLO("best.pt") 
+names = model.names #names is a list of class names corresponding to the class IDs used in the model.
+
+print(predict('Blister_Blight_dt3_00119.jpg'))
+#print(predict('Blister_Blight_dt3_00132.jpg'))
+
+
+
+
