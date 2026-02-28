4daae630dd19c1d9d4c7a56b386524d24a8275f2
Chinthaka-Sharuna <sharunadrc@gmail.com>
2026-02-28 09:45:49 +0530

    feat: Add disease detection functionality and update related files; include input data and logging enhancements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request,jsonify
-from tea_disease_identifier import predict
+from tea_disease_identifier import predict,claculate_infection_percentage
 import os
 
 
