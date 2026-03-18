cee06d7c52840f937ffc165407ae56c31c801391
Chinthaka-Sharuna <sharunadrc@gmail.com>
2026-03-18 22:10:21 +0530

    Clean Integration branch

--- bootstrap.py
+++
@@ -1,5 +0,0 @@
-from app.database.init_db import init_db
-from config import Config
-
-print(Config.DEFAULT_DB_NAME)
-init_db(Config.DEFAULT_DB_NAME,Config.DB_NAME,'app/database/init_db/create_tables.sql','app/database/init_db/test_data.sql')
